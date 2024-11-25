34420e25e9ea3e8a8c07754822350931478274f2
Kirk Truax <kirktruax93@proton.me>
2024-11-25 13:04:17 -0500

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+"""The purpose of this file is to create a simple hello world program to test the streamlit community cloud."""
+
+import streamlit as st
+
+st.title("Hello World")
+st.write("This is a simple hello world program to test the streamlit community cloud.")
